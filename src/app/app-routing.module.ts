00c5a95b37e84e7f1a2e5f207fe91d0d0040e278
Vadim Sokoltsov <vforvad@gmail.com>
2017-09-09 02:15:15 +0300

    implement simple routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CompaniesComponent } from './companies/companies.component';
+import { VacanciesComponent } from './vacancies/vacancies.component';
 
 const appRoutes: Routes = [
+  { path: '', component: CompaniesComponent },
+  { path: 'companies', component: CompaniesComponent },
+  { path: 'vacancies', component: VacanciesComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
 
 import {
   MdSidenavModule,
@@ -13,16 +14,21 @@ import {
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { VacanciesComponent } from './vacancies/vacancies.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SideNavComponent
+    SideNavComponent,
+    CompaniesComponent,
+    VacanciesComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    AppRoutingModule,
     MdSidenavModule,
     MdToolbarModule,
     MdIconModule,
