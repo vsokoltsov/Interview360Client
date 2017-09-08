e1dbfa13b3a2c7c740bea051ada89d1b2d07efc2
Vadim Sokoltsov <vforvad@gmail.com>
2017-09-08 22:58:57 +0300

    add some kind of the toggled navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MdDrawer } from '@angular/material';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  @ViewChild('sidenav') sidenav: MdDrawer;
+
+  toggleNavigation() {
+    this.sidenav.toggle();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {MdSidenavModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +11,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
