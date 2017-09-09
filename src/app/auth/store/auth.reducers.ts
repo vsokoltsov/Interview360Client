0047da2ea42ac569f07176e59d5b4972187443fb
Vadim Sokoltsov <vforvad@gmail.com>
2017-09-09 20:22:23 +0300

    add custom reducer

---
+++ src/app/auth/store/auth.reducers.ts
@@ -0,0 +1,29 @@
+import * as AuthActions from './auth.actions';
+
+export interface State {
+  token: string;
+  currentUser: {};
+  signInErrors: {};
+}
+
+const initialState: State = {
+  token: null,
+  currentUser: {},
+  signInErrors: {}
+};
+
+export function authReducer(state = initialState, action: AuthActions.AuthActions) {
+  switch(action.type) {
+    case AuthActions.SUCCESS_SIGN_IN:
+      return {
+        ...state,
+        token: action.payload
+      };
+    case AuthActions.FAILED_SIGN_IN:
+      return {
+        ...state,
+        signInErrors: action.payload
+      };
+    default: return state;
+  }
+}
