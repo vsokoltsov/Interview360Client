c8f4817ce97122bb2f22176c54afdd3310a63954
Vadim Sokoltsov <vforvad@gmail.com>
2017-09-09 20:18:04 +0300

    add custom actions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
---
+++ src/app/auth/store/auth.actions.ts
@@ -0,0 +1,20 @@
+import { Action } from '@ngrx/store';
+
+export const SUCCESS_SIGN_IN = 'SUCCESS_SIGN_IN';
+export const FAILED_SIGN_IN = 'FAILED_SIGN_IN';
+
+export class SuccessSignIn implements Action {
+  readonly type = SUCCESS_SIGN_IN;
+
+  constructor(public payload: { token: string }) {}
+}
+
+export class FailedSignIn implements Action {
+  readonly type = FAILED_SIGN_IN;
+
+  constructor(public payload: { errors: {} }) {}
+}
+
+export type AuthActions =
+SuccessSignIn |
+FailedSignIn;
