0673ddbd2f9697d6636e0d6ca80056d99fe8f560
Vadim Sokoltsov <vforvad@gmail.com>
2017-09-24 14:33:35 +0300

    change karma reporter

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,8 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
+      require('@angular/cli/plugins/karma'),
+      require('karma-verbose-reporter')
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -22,7 +23,7 @@ module.exports = function (config) {
     angularCli: {
       environment: 'dev'
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['verbose'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
