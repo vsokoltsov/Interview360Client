bc2dadf01088f9c932393b835155c01ce0c7c164
Vadim Sokoltsov <vforvad@gmail.com>
2017-09-24 12:43:44 +0300

    finally, figured out how to test profile view

--- src/app/profile/profile.component.spec.ts
+++ src/app/profile/profile.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { ProfileComponent } from './profile.component';
 import { By } from '@angular/platform-browser';
@@ -9,6 +10,7 @@ import { DebugElement }    from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { User } from '../auth/user.model';
 import { reducers } from '../store/app.reducers';
@@ -17,6 +19,13 @@ import * as fromApp from '../store/app.reducers';
 import * as AuthActions from '../auth/store/auth.actions';
 import { ApiService } from '../shared/api.service';
 import { ProfileService } from './profile.service'
+import { EditComponent } from './edit/edit.component';
+import { InfoComponent } from './edit/info/info.component';
+import { PasswordComponent } from './edit/password/password.component';
+import { profileRoutes } from './profile-routing.module';
+import { environment } from '../../environments/environment';
+
+const user = new User(1, 'example@mail.com', 'a', 'b');
 
 fdescribe('ProfileComponent', () => {
   let component: ProfileComponent;
@@ -25,28 +34,31 @@ fdescribe('ProfileComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        ProfileComponent,
+        ProfileComponent
       ],
       imports: [
         RouterTestingModule,
         StoreModule.forRoot(fromApp.reducers),
         HttpClientModule,
-        ReactiveFormsModule
+        HttpClientTestingModule
       ],
       providers: [
         CookieService,
         ApiService,
         ProfileService
-      ]
-    })
-    .compileComponents();
+      ],
+      schemas: [ NO_ERRORS_SCHEMA ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ProfileComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    let store = TestBed.get(Store);
+    const action = new AuthActions.CurrentUserReceived(user);
+    store.dispatch(action);
   }));
 
-  it('should be created', () => {
+  it('should be created', async(() => {
     expect(component).toBeTruthy();
-  });
+  }));
 });
