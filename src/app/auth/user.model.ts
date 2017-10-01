ad86d07bc6b4b3f1682748db3b9e23d87a55ae8d
Vadim Sokoltsov <vforvad@gmail.com>
2017-10-01 20:33:11 +0300

    add roles list to suer model

--- src/app/auth/user.model.ts
+++ src/app/auth/user.model.ts
@@ -4,12 +4,14 @@ export class User {
   public first_name: string;
   public last_name: string;
   public attachment: { url: string};
+  public roles: {}
 
   constructor(id: number, email: string,
-              first_name: string, last_name: string) {
+              first_name: string, last_name: string, roles: {} = null) {
     this.id = id;
     this.email = email;
     this.first_name = first_name;
     this.last_name = last_name;
+    this.roles = roles;
   }
 }
