a248554db546045daccd423149484cc2cb0115d2
Vadim Sokoltsov <vforvad@gmail.com>
2017-10-17 01:42:48 +0300

    add vacancy model

---
+++ src/app/vacancies/vacancy.model.ts
@@ -0,0 +1,22 @@
+import { Company } from '../companies/company.model';
+
+export class Vacancy {
+  public id: number;
+  public title: string;
+  public description: string;
+  public salary: number;
+  public active: boolean;
+  public skills: {}[];
+  public company: Company;
+
+  constructor(id?: number, title?: string, description?: string, salary?: number,
+              active?: boolean, skills = null, company: Company = null) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.salary = salary;
+    this.active = active;
+    this.skills = skills;
+    this.company = company;
+  }
+}
