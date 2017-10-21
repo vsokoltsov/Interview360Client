f593e8b21ac370033c7d3652e331b8903d8c95a7
Vadim Sokoltsov <vforvad@gmail.com>
2017-10-21 19:23:59 +0300

    add spec for vacancies list

--- src/app/vacancies/vacancies.component.spec.ts
+++ src/app/vacancies/vacancies.component.spec.ts
@@ -3,8 +3,15 @@ import {HttpClientModule} from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { StoreModule, Store } from '@ngrx/store';
 import { RouterTestingModule } from '@angular/router/testing';
+import {Router, ActivatedRoute, Params} from '@angular/router';
 import { NgxSvgIconModule } from 'ngx-svg-icon';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs';
+import 'rxjs/add/observable/of';
 
+import { environment } from '../../environments/environment';
+import { Vacancy } from './vacancy.model';
+import { Company } from '../companies/company.model';
 import { VacanciesComponent } from './vacancies.component';
 import { VacanciesListItemComponent } from './vacancies-list-item/vacancies-list-item.component';
 import { VacanciesService } from './vacancies.service';
@@ -13,9 +20,24 @@ import { CookieService } from 'angular2-cookie/core';
 import { AuthService } from '../auth/auth.service';
 import * as fromApp from '../store/app.reducers';
 
+const vacancy = new Vacancy(1, 'b', 'c');
+const company = new Company(1, 'a', 'b', '2017-08-19', 'a');
+const response = { companies: [
+  {
+    id: 1,
+    name: 'aaa',
+    description: 'awdawd',
+    start_date: '2017-08-19',
+    city: '1'
+  }
+]};
+
 describe('VacanciesComponent', () => {
   let component: VacanciesComponent;
   let fixture: ComponentFixture<VacanciesComponent>;
+  let httpMock: HttpTestingController;
+  let store: Store<fromApp.AppState>;
+  let vacanciesService: VacanciesService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -34,16 +56,32 @@ describe('VacanciesComponent', () => {
         ApiService,
         AuthService,
         CookieService,
-        VacanciesService
+        VacanciesService,
+        {
+          provide: ActivatedRoute, useValue: {
+            params: Observable.of({ companyId: company.id })
+          }
+        }
       ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(VacanciesComponent);
+    store = TestBed.get(Store);
+    vacanciesService = TestBed.get(VacanciesService);
+    spyOn(vacanciesService, 'loadList').and.callThrough();
     component = fixture.componentInstance;
+    httpMock = TestBed.get(HttpTestingController);
     fixture.detectChanges();
+    let result = httpMock.expectOne(`${environment.baseUrl}/companies/${company.id}/vacancies/`);
+    result.flush(response);
+    httpMock.verify();
   }));
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('call loadList on vacanciesService instance', () => {
+    expect(vacanciesService.loadList).toHaveBeenCalled();
   });
 });
