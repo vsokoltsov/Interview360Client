c2fdd871a761ae50bfdf04ab15b2cd29c8323580
Vadim Sokoltsov <vforvad@gmail.com>
2017-10-25 01:15:10 +0300

    implement interviews base component

---
+++ src/app/interviews/interviews.component.spec.ts
@@ -0,0 +1,57 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {HttpClientModule} from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { StoreModule, Store } from '@ngrx/store';
+import { RouterTestingModule } from '@angular/router/testing';
+import {Router, ActivatedRoute, Params} from '@angular/router';
+import { NgxSvgIconModule } from 'ngx-svg-icon';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs';
+import 'rxjs/add/observable/of';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import * as fromApp from '../store/app.reducers';
+
+import { environment } from '../../environments/environment';
+import { Vacancy } from '../vacancies/vacancy.model';
+import { Company } from '../companies/company.model';
+import { ApiService } from '../shared/api.service';
+import { CookieService } from 'angular2-cookie/core';
+
+import { InterviewsComponent } from './interviews.component';
+
+describe('InterviewsComponent', () => {
+  let component: InterviewsComponent;
+  let fixture: ComponentFixture<InterviewsComponent>;
+  let httpMock: HttpTestingController;
+  let store: Store<fromApp.AppState>;
+  // let vacanciesService: VacanciesService;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        InterviewsComponent
+      ],
+      imports: [
+        StoreModule.forRoot(fromApp.reducers),
+        RouterTestingModule,
+        NgxSvgIconModule,
+        HttpClientModule,
+        HttpClientTestingModule
+      ],
+      providers: [
+        ApiService,
+        CookieService,
+        // VacanciesService
+      ],
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(InterviewsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
