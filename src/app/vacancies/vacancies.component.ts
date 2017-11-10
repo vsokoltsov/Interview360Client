2f3f5d39d6fbabb53794b7ee828423119e112b15
Vadim Sokoltsov <vforvad@gmail.com>
2017-11-10 21:44:49 +0300

    add search of vacancies

--- src/app/vacancies/vacancies.component.ts
+++ src/app/vacancies/vacancies.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 import { Subscription } from 'rxjs/Subscription';
 import { Store } from '@ngrx/store';
 import {Router, ActivatedRoute, Params} from '@angular/router';
@@ -13,6 +14,7 @@ import * as fromApp from '../store/app.reducers';
   styleUrls: ['./vacancies.component.scss']
 })
 export class VacanciesComponent implements OnInit {
+  vacanciesSearchForm: FormGroup;
   vacancies: Vacancy[];
   subscription: Subscription;
   companyId: number;
@@ -22,6 +24,14 @@ export class VacanciesComponent implements OnInit {
               private vacanciesService: VacanciesService) { }
 
   ngOnInit() {
+    this.vacanciesSearchForm = new FormGroup({
+      'query': new FormControl(null)
+    });
+    this.vacanciesSearchForm.get('query').valueChanges.subscribe(
+      data => {
+        this.searchVacancies(data);
+      }
+    );
     this.activatedRoute.params.subscribe((params: Params) => {
         this.companyId = params['companyId'];
         this.vacanciesService.loadList(this.companyId);
@@ -34,6 +44,10 @@ export class VacanciesComponent implements OnInit {
         }
       }
     );
+  }
+
+  searchVacancies(query: string) {
+    this.vacanciesService.searchVacancies(this.companyId, query);
   }
 
 }
