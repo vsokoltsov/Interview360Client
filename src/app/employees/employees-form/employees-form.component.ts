b66e8fa7955d070ee66d81c25b40089f628609f7
Vadim Sokoltsov <vforvad@gmail.com>
2017-11-18 02:02:24 +0300

    add some kind of employee form component

--- src/app/employees/employees-form/employees-form.component.ts
+++ src/app/employees/employees-form/employees-form.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs/Subscription';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+
+import { EmployeesService } from '../employees.service';
+import * as fromApp from '../../store/app.reducers';
+import * as EmployeesActions from '../store/employees.actions';
 
 @Component({
   selector: 'app-employees-form',
@@ -6,10 +14,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employees-form.component.scss']
 })
 export class EmployeesFormComponent implements OnInit {
+  employeesForm: FormGroup;
 
-  constructor() { }
+  constructor(private store: Store<fromApp.AppState>,
+              private employeesService: EmployeesService) { }
 
   ngOnInit() {
+    this.employeesForm = new FormGroup({
+      'employees': new FormArray([])
+    });
+  }
+
+  submit() {
+    console.log(this.employeesForm.value);
+  }
+
+  addEmployee() {
+    const control = new FormGroup({
+      'email': new FormControl(null, [Validators.required]),
+      'role': new FormControl(null, [Validators.required])
+    });
+    (<FormArray>this.employeesForm.get('employees')).push(control);
+    console.log((<FormArray>this.employeesForm.get('employees')));
   }
 
 }
--- src/app/employees/employees-routing.module.ts
+++ src/app/employees/employees-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { EmployeesComponent } from './employees.component';
 import { EmployeesEmptyComponent } from './employees-empty/employees-empty.component';
+import { EmployeesFormComponent } from './employees-form/employees-form.component';
 
 export const employeesRoutes: Routes = [
   { path: '', component: EmployeesComponent, children: [
-      { path: '', component: EmployeesEmptyComponent },
+    { path: 'new', component: EmployeesFormComponent, pathMatch: 'full' },
+    { path: '', component: EmployeesEmptyComponent },
   ] }
 ];
 
