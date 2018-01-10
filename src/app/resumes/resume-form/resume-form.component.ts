ba77ff657202e2ed1b0e79debaa09237eabdc797
Vadim Sokoltsov <vforvad@gmail.com>
2018-01-11 01:37:20 +0300

    add value for the resume-form component

--- src/app/resumes/resume-form/resume-form.component.ts
+++ src/app/resumes/resume-form/resume-form.component.ts
@@ -6,6 +6,9 @@ import {Router, ActivatedRoute, Params} from '@angular/router';
 
 import { Resume } from '../resume.model';
 import { User } from '../../auth/user.model';
+import { Skill } from '../../vacancies/skill.model';
+import * as fromApp from '../../store/app.reducers';
+import * as ResumesActions from '../store/resumes.actions';
 
 @Component({
   selector: 'app-resume-form',
@@ -13,10 +16,46 @@ import { User } from '../../auth/user.model';
   styleUrls: ['./resume-form.component.scss']
 })
 export class ResumeFormComponent implements OnInit {
+  resumeForm: FormGroup;
 
-  constructor() { }
+  constructor(private store: Store<fromApp.AppState>,
+              private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.resumeForm = new FormGroup({
+      'title': new FormControl(null, [Validators.required]),
+      'description': new FormControl(null, [Validators.required]),
+      'skills': new FormArray([])
+    });
   }
 
+  addSkill(skill: Skill) {
+    let skills = this.resumeForm.value.skills.map(item => item.id);
+    if (!skills.includes(skill.id)) {
+        this.setSkillToForm(skill);
+    }
+  }
+
+  deleteSkill(skill: Skill) {
+    const idx = this.resumeForm.value.skills.findIndex(
+      item => item.id == skill.id
+    );
+    if (idx !== -1) {
+      (<FormArray>this.resumeForm.get('skills')).removeAt(idx);
+    }
+  }
+
+  private setSkills(detail: Resume) {
+    if (detail.skills) {
+      const skillsArr = detail.skills.map(item => {
+        return new FormControl(item, Validators.required);
+      });
+      this.resumeForm.setControl('skills', new FormArray(skillsArr));
+    }
+  }
+
+  private setSkillToForm(skill: Skill) {
+    const control = new FormControl(skill, Validators.required);
+    (<FormArray>this.resumeForm.get('skills')).push(control);
+  }
 }
