2e05736cbdde81050d884e97ed1576ac396a315b
Vadim Sokoltsov <vforvad@gmail.com>
2018-01-21 00:18:09 +0300

    boilerplate for the workplace form

--- src/app/resumes/workplaces/workplaces-form/workplaces-form.component.ts
+++ src/app/resumes/workplaces/workplaces-form/workplaces-form.component.ts
@@ -4,12 +4,14 @@ import { Store } from '@ngrx/store';
 import { Location } from '@angular/common';
 import { Subscription } from 'rxjs/Subscription';
 import {Router, ActivatedRoute, Params} from '@angular/router';
+import { IDatePickerConfig } from 'ng2-date-picker';
 
 import { Resume } from '../../resume.model';
 import { User } from '../../../auth/user.model';
 import { Skill } from '../../../shared/skills/skill.model';
 import { SkillsService } from '../../../shared/skills/skills.service';
 import { ResumesService } from '../../resumes.service';
+import { CompaniesService } from '../../../companies/companies.service';
 import * as fromApp from '../../../store/app.reducers';
 import * as ResumesActions from '../../store/resumes.actions';
 
@@ -19,10 +21,37 @@ import * as ResumesActions from '../../store/resumes.actions';
   styleUrls: ['./workplaces-form.component.scss']
 })
 export class WorkplacesFormComponent implements OnInit {
+  workplacesForm: FormGroup;
+  datePickerConfig: IDatePickerConfig = {
+    format: 'YYYY-MM-DD',
+    showSeconds: false,
+    showTwentyFourHours: true
+  };
 
   constructor() { }
 
   ngOnInit() {
+    this.workplacesForm = new FormGroup({
+      'workplaces': new FormArray([])
+    });
   }
 
+  addNewWorkplace() {
+    const control = new FormGroup({
+      'company': new FormControl(null, [Validators.required]),
+      'position': new FormControl(null, [Validators.required]),
+      'description': new FormControl(null, [Validators.required]),
+      'start_date': new FormControl(null, [Validators.required]),
+      'end_date': new FormControl(null, [Validators.required])
+    });
+    (<FormArray>this.workplacesForm.get('workplaces')).push(control);
+  }
+
+  getWorkplaces(form) {
+    return form.get('workplaces').controls;
+  }
+
+  submit() {
+
+  }
 }
