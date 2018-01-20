26a80953156c27ae05e377ef8bc60a19cf329903
Vadim Sokoltsov <vforvad@gmail.com>
2018-01-20 21:50:23 +0300

    add link to workplaces component

--- src/app/resumes/workplaces/workplaces-form/workplaces-form.component.ts
+++ src/app/resumes/workplaces/workplaces-form/workplaces-form.component.ts
@@ -1,4 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, DoCheck } from '@angular/core';
+import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { Location } from '@angular/common';
+import { Subscription } from 'rxjs/Subscription';
+import {Router, ActivatedRoute, Params} from '@angular/router';
+
+import { Resume } from '../../resume.model';
+import { User } from '../../../auth/user.model';
+import { Skill } from '../../../shared/skills/skill.model';
+import { SkillsService } from '../../../shared/skills/skills.service';
+import { ResumesService } from '../../resumes.service';
+import * as fromApp from '../../../store/app.reducers';
+import * as ResumesActions from '../../store/resumes.actions';
 
 @Component({
   selector: 'app-workplaces-form',
