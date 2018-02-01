7968429d697efbbeb66137d9722fc42588e80b35
Vadim Sokoltsov <vforvad@gmail.com>
2018-02-01 20:01:26 +0300

    add contact form to the router

--- src/app/resumes/resumes-routing.module.ts
+++ src/app/resumes/resumes-routing.module.ts
@@ -5,14 +5,17 @@ import { ResumesComponent } from './resumes.component';
 import { ResumeFormComponent } from './resume-form/resume-form.component';
 import { ResumesDetailComponent } from './resumes-detail/resumes-detail.component';
 import { WorkplacesFormComponent } from './workplaces/workplaces-form/workplaces-form.component';
+import { ContactFormComponent } from './contacts/contact-form/contact-form.component';
 
 export const resumesRoutes: Routes = [
  { path: '', component: ResumesComponent },
  { path: 'new', component: ResumeFormComponent },
  { path: 'new/workplaces', component: WorkplacesFormComponent },
+ { path: 'new/contacts', component: ContactFormComponent },
  { path: ':id', component: ResumesDetailComponent },
  { path: ':id/edit', component: ResumeFormComponent },
- { path: ':id/edit/workplaces', component: WorkplacesFormComponent }
+ { path: ':id/edit/workplaces', component: WorkplacesFormComponent },
+ { path: ':id/edit/contacts', component: ContactFormComponent }
 ];
 
 
