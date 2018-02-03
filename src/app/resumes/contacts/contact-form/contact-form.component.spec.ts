a88c20f6ab0cba33df59f34ef944ca9841d251ec
Vadim Sokoltsov <vforvad@gmail.com>
2018-02-03 19:29:24 +0300

    fix specs for the resume contact form component

--- src/app/resumes/contacts/contact-form/contact-form.component.spec.ts
+++ src/app/resumes/contacts/contact-form/contact-form.component.spec.ts
@@ -1,6 +1,25 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { StoreModule, Store } from '@ngrx/store';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import {Router, ActivatedRoute, Params} from '@angular/router';
+import { NgxSvgIconModule } from 'ngx-svg-icon';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { CookieService } from 'angular2-cookie/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs';
+import 'rxjs/add/observable/of';
 
 import { ContactFormComponent } from './contact-form.component';
+import { PopupNotificationsService } from '../../../popup-notifications/popup-notifications.service';
+import { Resume } from '../../resume.model';
+import { ResumesService } from '../../resumes.service';
+import { environment } from '../../../../environments/environment';
+import { ApiService } from '../../../shared/api.service';
+import { AuthService } from '../../../auth/auth.service';
+import * as fromApp from '../../../store/app.reducers';
 
 describe('ContactFormComponent', () => {
   let component: ContactFormComponent;
@@ -8,7 +27,24 @@ describe('ContactFormComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ContactFormComponent ]
+      declarations: [
+        ContactFormComponent
+      ],
+      imports: [
+        StoreModule.forRoot(fromApp.reducers),
+        ReactiveFormsModule,
+        RouterTestingModule,
+        NgxSvgIconModule,
+        HttpClientModule,
+        HttpClientTestingModule
+      ],
+      providers: [
+        ApiService,
+        AuthService,
+        CookieService,
+        ResumesService,
+        PopupNotificationsService
+      ]
     })
     .compileComponents();
   }));
