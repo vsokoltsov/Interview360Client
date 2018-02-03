1f09e62038d7f35aa88b75b1a11c4313b0536ddc
Vadim Sokoltsov <vforvad@gmail.com>
2018-02-03 20:48:47 +0300

    fix specs for the resume form

--- src/app/resumes/resume-form/resume-form.component.spec.ts
+++ src/app/resumes/resume-form/resume-form.component.spec.ts
@@ -1,6 +1,27 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { StoreModule, Store } from '@ngrx/store';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import {Router, ActivatedRoute, Params} from '@angular/router';
+import { NgxSvgIconModule } from 'ngx-svg-icon';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { CookieService } from 'angular2-cookie/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs';
+import 'rxjs/add/observable/of';
 
 import { ResumeFormComponent } from './resume-form.component';
+import { PopupNotificationsService } from '../../popup-notifications/popup-notifications.service';
+import { Resume } from '../resume.model';
+import { ResumesService } from '../resumes.service';
+import { environment } from '../../../environments/environment';
+import { ApiService } from '../../shared/api.service';
+import { SkillsService } from '../../shared/skills/skills.service';
+import { AuthService } from '../../auth/auth.service';
+import * as fromApp from '../../store/app.reducers';
+import { AutocompleteModule } from '../../shared/autocomplete/autocomplete.module';
 
 describe('ResumeFormComponent', () => {
   let component: ResumeFormComponent;
@@ -8,7 +29,26 @@ describe('ResumeFormComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ResumeFormComponent ]
+      declarations: [
+        ResumeFormComponent
+      ],
+      imports: [
+        StoreModule.forRoot(fromApp.reducers),
+        ReactiveFormsModule,
+        RouterTestingModule,
+        NgxSvgIconModule,
+        HttpClientModule,
+        HttpClientTestingModule,
+        AutocompleteModule
+      ],
+      providers: [
+        ApiService,
+        AuthService,
+        CookieService,
+        ResumesService,
+        PopupNotificationsService,
+        SkillsService
+      ]
     })
     .compileComponents();
   }));
