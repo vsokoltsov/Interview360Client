5412c658dbc081e3dcbef3a26f6d21ab82ac00a0
Vadim Sokoltsov <vforvad@gmail.com>
2018-03-12 00:03:37 +0300

    implement spinner loader for the uploader component

--- src/app/shared/uploader/uploader.module.ts
+++ src/app/shared/uploader/uploader.module.ts
@@ -3,13 +3,16 @@ import { FileUploadModule } from 'ng2-file-upload/ng2-file-upload';
 import { UploaderComponent } from './uploader.component';
 import { CommonModule } from '@angular/common';
 
+import { LoaderModule } from '../loader/loader.module';
+
 @NgModule({
   declarations: [
     UploaderComponent
   ],
   imports: [
     CommonModule,
-    FileUploadModule
+    FileUploadModule,
+    LoaderModule
   ],
   exports: [
     UploaderComponent, CommonModule, FileUploadModule
