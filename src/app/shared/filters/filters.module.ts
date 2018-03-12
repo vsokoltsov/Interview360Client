00770887430811b07b8329e89f8f89a734a8c0d7
Vadim Sokoltsov <vforvad@gmail.com>
2018-03-12 23:13:35 +0300

    add select component filter to the filters module

--- src/app/shared/filters/filters.module.ts
+++ src/app/shared/filters/filters.module.ts
@@ -1,19 +1,24 @@
 import { NgModule } from '@angular/core';
 import { NgxSvgIconModule } from 'ngx-svg-icon';
 import { CommonModule } from '@angular/common';
+import { NgSelectModule } from '@ng-select/ng-select';
 
 import { OrderComponent } from './order/order.component';
+import { SelectComponent } from './select/select.component';
 
 @NgModule({
   declarations: [
-    OrderComponent
+    OrderComponent,
+    SelectComponent
   ],
   imports: [
     CommonModule,
-    NgxSvgIconModule
+    NgxSvgIconModule,
+    NgSelectModule
   ],
   exports: [
-    OrderComponent
+    OrderComponent,
+    SelectComponent
   ]
 })
 export class FiltersModule {}
